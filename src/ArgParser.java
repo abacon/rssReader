bd3f273ae38e2e7ff0530084f9de870905b6c3de
Veronica Lynn <me@veronicaelynn.com>
2013-04-15 18:49:44 -0500

    Commented ArgParser

--- src/ArgParser.java
+++ src/ArgParser.java
@@ -7,37 +7,37 @@ import java.util.Date;
 import java.util.regex.Pattern;
 
 /**
- * A simple class demonstrating the use of the Options class in the plume library,
- * for processing of command line arguments.
+ * A class to parse the command line arguments for the RSSReader. 
+ * 
+ * Derived from code by
  * @author Amy Csizmar Dalal
  * CS 204, Spring 2013
  * date: 9 April 2013
  */
 
-
-/*
-feeds, the file containing the list of feed URLs. This is the mandatory command line argument.
---by-date to display the story links in order of date posted, from newest to oldest (not sorted by feed)
---by-alpha to display the story links ordered by feed, with the feeds ordered alphabetically. (The default is to list the feeds in the order in which they're listed in the feed file.)
---number NUM to display the latest NUM story links in each feed
---since DATE to display the story links in each feed published on or after the specified date. DATE must be in the form yyyy-mm-dd (e.g. 2013-04-04 for April 4, 2013)
---title REGEX to only display stories whose titles match the regular expression REGEX
---description {on|off} to also display the description associated with a story. The default is off.
-[challenge problem] --newest to only display the stories posted or updated since the last time the feed parser was run.
-*/
-@OneOf({"byDate", "byAlpha"})
+@AtMostOneOf({"byDate", "byAlpha"})
 public class ArgParser {
 	// These instance variables are derived from the command line arguments passed in.
-	// Note that default values are assigned here as well.
-
-	private boolean byDate, byAlpha, newest, description;
-
-
+	
+	/** Flag to display story links in order of date posted */
+	private boolean byDate;
+	
+	/** Flag to display story links with feeds ordered alphabetically */
+	private boolean byAlpha;
+	
+	/** Flag to display only stories posted or updated since the last time the feed parser was run */
+	private boolean newest;
+	
+	/** Flag to also display the description associated with a story */
+	private boolean description;
+	
 	/** The number of stories per feed to list */
 	private int number;
 
+	/** Display only story links published on or after this date */
 	private Date since;
 
+	/** A regex to find matching titles */
 	private Pattern title;
 
 	/** The file containing the list of feeds */
@@ -53,107 +53,109 @@ public class ArgParser {
 		newest = false;
 		description = false;
 		number = 2;
-		since = new Date();
+		since = new Date(0);
 		title = null;
-		filename = "";
+		filename = null;
 	}
 
 	/**
-	 * Retrieves the current value of the "sort by author" flag.
-	 * @return true if we should sort the stories by author, false otherwise
+	 * Retrieves the current value of the "sort by date" flag.
+	 * @return true if we should sort the posts by date, false otherwise
 	 */
 	public boolean isByDate() {
 		return byDate;
 	}
 
 	/**
-	 * Turn on the "sort by author" flag.
+	 * Turn on the "sort by date" flag.
+	 * Automatically turns off "sort by alpha" flag.
 	 */
-	@Option(description="sort by author")
+	@Option(description="sort by date")
 	public void setByDate() {
 		this.byDate = true;
 		this.byAlpha = false;
 	}
 
 	/**
-	 * Retrieves the current value of the "sort by author" flag.
-	 * @return true if we should sort the stories by author, false otherwise
+	 * Retrieves the current value of the "sort by alpha" flag.
+	 * @return true if we should sort the posts alphabetically, false otherwise
 	 */
 	public boolean isByAlpha() {
 		return byAlpha;
 	}
 
 	/**
-	 * Turn on the "sort by author" flag.
+	 * Turn on the "sort by alpha" flag.
+	 * Automatically turns off the "sort by date" flag.
 	 */
-	@Option(description="sort by author")
+	@Option(description="sort by alpha")
 	public void setByAlpha() {
 		this.byAlpha = true;
 		this.byDate = false;
 	}
 
 	/**
-	 * Retrieves the current value of the "sort by author" flag.
-	 * @return true if we should sort the stories by author, false otherwise
+	 * Retrieves the current value of the "show newest" flag.
+	 * @return true if we should only display the stories posted or updated since the last time the feed parser was run, false otherwise
 	 */
 	public boolean isNewest() {
 		return newest;
 	}
 
 	/**
-	 * Turn on the "sort by author" flag.
+	 * Turn on the "show newest" flag.
 	 */
-	@Option(description="sort by author")
+	@Option(description="show newest")
 	public void setNewest() {
 		this.newest = true;
 	}
 
 	/**
-	 * Retrieves the current value of the "sort by author" flag.
-	 * @return true if we should sort the stories by author, false otherwise
+	 * Retrieves the current value of the "show description" flag.
+	 * @return true if we should display the post's description, false otherwise
 	 */
 	public boolean isDescription() {
 		return description;
 	}
 
 	/**
-	 * Turn on the "sort by author" flag.
+	 * Turn on the "show description" flag.
 	 */
-	@Option(description="sort by author")
+	@Option(description="show description")
 	public void setDescription() {
 		this.description = true;
 	}
 
 	/**
-	 * Returns the number of stories per feed to display.
-	 * @return the number of stories to display
+	 * Returns the number of posts per feed to display.
+	 * @return the number of posts to display
 	 */
 	public int getNumber() {
 		return number;
 	}
 
 	/**
-	 * Sets the number of titles to display
-	 * @param num the number of titles
+	 * Sets the number of posts per feed to display
+	 * @param number the number of posts
 	 */
-	@Option(description="<N> the number of titles to display")
+	@Option(description="<N> the number of posts per feed to display")
 	public void setNumber(int number) {
 		this.number = number;
 	}
 
 	/**
-	 * Returns the number of stories per feed to display.
-	 * @return the number of stories to display
+	 * Returns the date for which to display posts published on or after.
+	 * @return the date for which to display posts published on or after
 	 */
 	public Date getSince() {
 		return since;
 	}
 
 	/**
-	 * Sets the number of titles to display
-	 * @param num the number of titles
+	 * Sets the date for which to display posts published on or after
+	 * @param since the date for which to display posts published on or after
 	 */
-	@Option(description="<N> the number of titles to display")
+	@Option(description="<S> the date for which to display posts published on or after")
 	public void setSince(String since) {
 		DateFormat df = new SimpleDateFormat("yyyy-mm-dd");
 		try {
@@ -164,18 +166,18 @@ public class ArgParser {
 	}
 
 		/**
-	 * Returns the number of stories per feed to display.
-	 * @return the number of stories to display
+	 * Returns the REGEX with which to match feed titles.
+	 * @return the REGEX for title matching
 	 */
 	public Pattern getTitle() {
 		return title;
 	}
 
 	/**
-	 * Sets the number of titles to display
-	 * @param num the number of titles
+	 * Sets the REGEX with which to match feed titles.
+	 * @param title the string to be parsed into a REGEX for title matching
 	 */
-	@Option(description="<N> the number of titles to display")
+	@Option(description="<S> the regex with which to match feed titles")
 	public void setTitle(String title) {
 		this.title = Pattern.compile(title);
 	}
@@ -188,6 +190,10 @@ public class ArgParser {
 		this.filename = filename;
 	}
 	
+	/**
+	 * Returns the filename containing the feeds
+	 * @return the filename containing the feeds
+	 */
 	public String getFilename() {
 		return filename;
 	}
