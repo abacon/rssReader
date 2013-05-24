1f30f473aa1e58b182226550e42c3d48613fb097
Veronica Lynn <me@veronicaelynn.com>
2013-05-24 03:06:50 -0500

    TitleAndDescObserver commented

--- src/TitleAndDescObserver.java
+++ src/TitleAndDescObserver.java
@@ -6,19 +6,35 @@ import org.jsoup.Jsoup;
 import com.sun.syndication.feed.synd.SyndEntryImpl;
 import com.sun.syndication.feed.synd.SyndFeedImpl;
 
-
+/**
+ * TitleAndDescObserver displays the title and description of new posts when they are detected.
+ * Use it by adding it to a FeedWatcher instance.
+ * 
+ * @see FeedWatcher#addObserver(FeedObserver)
+ */
 public class TitleAndDescObserver extends FeedObserver {
 
 	private static final String TERMINAL_BLACK = "\033[0m";
 	private static final String TERMINAL_BOLD = "\033[1m";
 	private Date lastRunDate;
 	
+	/**
+	* Notify the observer that a feed has been updated
+	*
+	* @param changedFeeds   the list of feeds that have updated
+	* @param lastRunDAte    when the FeedWatcher last checked for updates
+	*/
 	@Override
 	public void notify(ArrayList<SyndFeedImpl> changedFeeds, Date lastRunDate) {
 		this.lastRunDate = lastRunDate;
 		displayChangedFeeds(changedFeeds);
 	}
 
+    /**
+    * Display the feeds that have changed since the last time notify was called
+    * 
+    * @param changedFeeds   The list of foods to display
+    */
 	protected void displayChangedFeeds(ArrayList<SyndFeedImpl> changedFeeds) {
 		
 		for (SyndFeedImpl feed : changedFeeds) {
@@ -28,6 +44,9 @@ public class TitleAndDescObserver extends FeedObserver {
 		}
 	}
 	
+	/**
+	* Display a single entry, properly formatted
+	*/
 	protected void printEntry(SyndEntryImpl entry) { 
 		System.out.println(TERMINAL_BOLD + entry.getTitle());
 		System.out.println(TERMINAL_BLACK + stripHTML(entry.getDescription().getValue()));
